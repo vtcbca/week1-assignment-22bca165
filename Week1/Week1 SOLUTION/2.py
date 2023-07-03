32986f6727d07fa35bad81d133de484bd72a91e9
22bca165 <127925833+22bca165@users.noreply.github.com>
2023-07-03 13:05:13 +0530

    Update 2.py

--- Week1/Week1 SOLUTION/2.py
+++ Week1/Week1 SOLUTION/2.py
@@ -1,11 +1,9 @@
 #Write a python script to enter any number and print the sum of its digit.
 def sumofitsdigit(n):
     sum=0
-    while n>0:
-        r=n%10
-        sum=sum+r
-        n=n//10
-    print("Sum of digit is {}".format(sum))
+    for i in str(n):
+        sum=sum+int(i)
+    print("Sum of digit is {}.".format(sum))
 
 n=int(input("Enter any number:"))
 sumofitsdigit(n)
